9b484abd916c038d468af1a57e00d6ed9f9a462c
olu-baba76495 <olu76495@yahoo.com>
2020-08-14 19:15:58 +0100

    NO-JIRA - Amended feature file and created Step Definition

---
+++ src/test/java/co/uk/zoopla/stepDefinitions/ForSaleSearchZooplaSteps.java
@@ -0,0 +1,59 @@
+package co.uk.zoopla.stepDefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ForSaleSearchZooplaSteps {
+
+    @Given("I navigate to zoopla homepage")
+    public void i_navigate_to_zoopla_homepage() {
+
+    }
+
+    @When("I enter {string} into the search field")
+    public void i_enter_into_the_search_field(String string) {
+
+    }
+
+    @When("I select {string} from the Min Price dropdown")
+    public void i_select_from_the_min_price_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from the Max Price dropdown")
+    public void i_select_from_the_max_price_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from Property type dropdown")
+    public void i_select_from_property_type_dropdown(String string) {
+
+    }
+
+    @When("I select {string} from Bedrooms dropdown")
+    public void i_select_from_bedrooms_dropdown(String string) {
+
+    }
+
+    @When("I click on Search button")
+    public void i_click_on_search_button() {
+
+    }
+
+    @Then("search result page is displayed with relevant results")
+    public void search_result_page_is_displayed_with_relevant_results() {
+
+    }
+
+    @Then("an error message is displayed on search field")
+    public void an_error_message_is_displayed_on_search_field() {
+
+    }
+
+    @Then("an error page is displayed")
+    public void an_error_page_is_displayed() {
+
+    }
+}
